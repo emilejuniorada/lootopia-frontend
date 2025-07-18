3a6e932bee5f0c8a5272fd43b04dbb7d72ef17df
Emile Junior ADA <junada99@gmail.com>
2025-07-18 12:43:18 +0200

    Fixing error on Draw not existing

--- components/dashboard/hunts/maps/DrawControl.tsx
+++ components/dashboard/hunts/maps/DrawControl.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
-import L from 'leaflet';
 import 'leaflet-draw';
 import { Map } from '@/stores/quest';
 
+declare const L: any; // --> Works
+import 'leaflet-draw';
 const DrawControl = ({ setMap }: { setMap?: (map: Map) => void }) => {
     const map = useMap();
 
